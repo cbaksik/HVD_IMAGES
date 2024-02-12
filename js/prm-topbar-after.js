5fcf08fc97ba0bd5e3556d23079c4674b23b45d6
mferrarini <maura_ferrarini@harvard.edu>
2024-02-12 14:12:18 -0500

    Revert "removed references to an old header"
    This reverts commit 61b3e9a0525447f252d6346bcfed3bf8aa06c3c1.

---
+++ js/prm-topbar-after.js
@@ -0,0 +1,57 @@
+/**
+ * Created by samsan on 6/29/17.
+ */
+
+(function () {
+
+    angular.module('viewCustom')
+    .controller('prmTopbarAfterController', ['$element','prmSearchService','$scope','$compile', function ($element,prmSearchService, $scope, $compile) {
+
+        let vm = this;
+        let cs = prmSearchService;
+
+        // get rest endpoint Url
+        vm.getUrl=function () {
+            var configFile = cs.getEnv();
+            cs.getAjax('/primo-explore/custom/HVD_IMAGES/html/'+configFile,'','get')
+                .then(function (res) {
+                        vm.api=res.data;
+                        cs.setApi(vm.api);
+                    },
+                    function (error) {
+                        console.log(error);
+                    }
+                )
+        };
+
+
+        vm.$onInit=function() {
+            // hide primo tab menu
+            vm.parentCtrl.showMainMenu=false;
+            // create new div for the top white menu
+            let primoExplore = document.getElementsByTagName('primo-explore')[0];
+            let div=document.createElement('div');
+            div.setAttribute('id','customTopMenu');
+            div.setAttribute('class','topMenu');
+            // create custom top white bar
+            let customTop = document.createElement('custom-top-menu');
+            div.appendChild(customTop);
+            if(primoExplore.children[0].className !== 'topMenu') {
+                $compile(div)($scope);
+                primoExplore.prepend(div);
+            }
+
+            vm.getUrl();
+
+        };
+
+    }]);
+
+
+    angular.module('viewCustom')
+    .component('prmTopbarAfter', {
+        bindings: {parentCtrl: '<'},
+        controller: 'prmTopbarAfterController'
+    });
+
+})();
