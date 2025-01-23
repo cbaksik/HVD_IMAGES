d5bf654a9b8294a326120c5e80bfb615ab8e0285
Corinna Baksik <cbaksik@gmail.com>
2025-01-23 16:57:24 -0500

    hvd_SubjectName/Place for component display

--- js/custom-map-xml-keys.js
+++ js/custom-map-xml-keys.js
@@ -1,6 +1,10 @@
 /**
  * Created by samsan on 9/28/17.
  * This service find out the xml key, sort them, display them in order
+ * 
+ * ********************* IMPORTANT *************************
+ * 
+ * in addition to adding new variables to keys list, you must also add them to the sort order for components (omit the hvd_)
  */
 
 (function () {
@@ -25,6 +29,8 @@
             {'lds30':'Form / Genre'},
             {'lds31':'Place'},
             {'lds44':'Associated Name'},
+            {'lds36':'Is part of'},
+            {'lds37':'Contains'},
             {'associatedName':'Associated Name'},
             {'creationdate':'Creation Date'},
             {'creator':'Author / Creator'},
@@ -61,6 +67,8 @@
             {'hvd_style':'Image Style'},
             {'hvd_title':'Image Title'},
             {'hvd_topic':'Image Subject'},
+            {'hvd_subjectName':'Image Subject Name'},
+            {'hvd_subjectPlace':'Image Subject Place'},
             {'hvd_useRestrictions':'Image Use Restrictions'},
             {'hvd_workType':'Image Type'},
             {'_attr':'Image ID'},
@@ -107,7 +115,7 @@
         // re-arrange sorting component order
         serviceObj.orderList=['title','creator','freeDate','state','production','description','physicalDescription','materials','dimensions',
         'notes','note','topic','placeName','location','culture','style','workType','classification','itemIdentifier',
-            'associatedName','relatedWork','relatedInformation','useRestrictions','copyright','repository'];
+            'associatedName','relatedWork','relatedInformation','useRestrictions','copyright','repository','subjectName','subjectPlace'];
         serviceObj.getOrderList=function (listKey) {
             var keys=[];
             var hvdKeys=[];
